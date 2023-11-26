22760f0a816c4d297fcca1cc47ef6bf833efe3bd
yagompassos <yagompassos@gmail.com>
2023-11-26 18:57:31 -0300

    criando componente de voo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VooComponent } from './components/voo/voo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'voo/', component: VooComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { VooComponent } from './components/voo/voo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VooComponent
   ],
   imports: [
     BrowserModule,
